df10ea02d25c5f6e982bb812d16cbd1906b63471
Rocktiel <seymensami81@hotmail.com>
2025-08-29 20:19:21 +0300

    feat: Button componenti yazıldı.

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
+  standalone: true,
   imports: [],
   templateUrl: './button.html',
-  styleUrl: './button.css'
+  styleUrl: './button.css',
 })
 export class Button {
+  @Input() text: string = 'Click me';
+  @Input() backgroundColor: string = '#4F46E5';
+  @Input() textColor: string = '#FFFFFF';
+  @Input() disabled: boolean = false;
 
+  @Output() onClick = new EventEmitter<void>();
+
+  handleClick() {
+    if (!this.disabled) {
+      this.onClick.emit();
+    }
+  }
 }
